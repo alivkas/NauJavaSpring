bac98288920bd0378b550899fe4c625948ffe8bd
matvey_elovskikh <mr1504dj@mail.ru>
2025-03-16 22:27:37 +0500

    init

---
+++ src/main/java/ru/matveyelovskikh/naujavaspring/NauJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.matveyelovskikh.naujavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NauJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/matveyelovskikh/naujavaspring/NauJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.matveyelovskikh.naujavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
